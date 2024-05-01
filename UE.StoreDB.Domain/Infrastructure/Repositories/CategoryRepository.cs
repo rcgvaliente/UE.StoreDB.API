699a3b6cd26bd4d08a0ecff48b85337f3a76c19c
Richard Gutierrez <mcarlos_25@hotmail.com>
2024-05-01 17:41:53 -0500

    Agregar archivos de proyecto.

---
+++ UE.StoreDB.Domain/Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UE.StoreDB.Domain.Core.Entities;
+using UE.StoreDB.Domain.Infrastructure.Data;
+
+namespace UE.StoreDB.Domain.Infrastructure.Repositories
+{
+    public class CategoryRepository
+    {
+        private readonly StoreDbContext _dbContext;
+
+        public CategoryRepository(StoreDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        //Sincrono: peticio FIFO
+        //public IEnumerable<Category> GetAll()
+        //{
+        //    return _dbContext
+        //            .Category
+        //            .Where(c=> c.IsActive == true)
+        //            .ToList();
+        //}
+        public async Task<IEnumerable<Category>> GetAll()
+        {
+            return await _dbContext
+                    .Category
+                    .Where(c => c.IsActive == true)
+                    .ToListAsync();
+        }
+        public async Task<Category> GetById(int id) 
+        {
+            return await _dbContext
+                    .Category
+                    .Where(c => c.IsActive == true && c.Id == id)
+                    .FirstOrDefaultAsync();
+
+        }
+
+    }
+}
